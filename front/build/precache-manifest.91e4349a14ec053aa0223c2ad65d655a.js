c642bd503cba7a5165b76e89bcf810aa7a646c82
Juan arango <jd.arango@uniandes.edu.co>
2020-05-10 14:59:51 -0500

    jeje

---
+++ front/build/precache-manifest.91e4349a14ec053aa0223c2ad65d655a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d1438eacce9cb31dfcffb736af992e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8548f5f6d73c3427a1e4",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "cc78620461a064ecfec0",
+    "url": "/static/css/main.62d00060.chunk.css"
+  },
+  {
+    "revision": "8548f5f6d73c3427a1e4",
+    "url": "/static/js/2.d9f7768e.chunk.js"
+  },
+  {
+    "revision": "e463d997dbecd21cff98855ac250f1ef",
+    "url": "/static/js/2.d9f7768e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc78620461a064ecfec0",
+    "url": "/static/js/main.30aafe27.chunk.js"
+  },
+  {
+    "revision": "7d8d15fd9a7998780fca",
+    "url": "/static/js/runtime-main.230851a4.js"
+  },
+  {
+    "revision": "20dda09227b43cc0c97fe36bf31f3f24",
+    "url": "/static/media/prueba2.20dda092.jpg"
+  },
+  {
+    "revision": "17c504d33cb502f284d09f56408b654b",
+    "url": "/static/media/reservas.17c504d3.jpg"
+  }
+]);
